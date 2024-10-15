e532c2cd1d8113e036086d11752f683f36ef4f02
Mangesh2004 <160419306+Mangesh2004@users.noreply.github.com>
2024-10-15 15:42:34 +0530

    Update question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -1,30 +1,62 @@
 #include <iostream>
 using namespace std;
-class Complex{
-    public: 
-    int real , img;
-    public:
-    void accept(){
-        cout<<"Enter the real no:"<<endl;
-        cin>>real;
-        cout<<"Enter the img no:"<<endl;
-        cin>>img;
-    };
-    Complex operator + ( Complex c){
+
+class Complex {
+public:
+    int real, img;
+
+    void accept() {
+        cout << "Enter the real part: ";
+        cin >> real;
+        cout << "Enter the imaginary part: ";
+        cin >> img;
+    }
+
+    Complex operator + (Complex c) {
         Complex c3;
-        c3.real=c.real+real;
-        c3.img=c.img+img;
+        c3.real = real + c.real;
+        c3.img = img + c.img;
         return c3;
-    };
-    void Display(){
-        cout<<real<<"+"<<img<<"i"<<endl;
-    };
+    }
+
+    Complex operator * (Complex c) {
+        Complex c3;
+        c3.real = (real * c.real) - (img * c.img);
+        c3.img = (real * c.img) + (img * c.real);
+        return c3;
+    }
+
+    void Display() {
+        cout << real << " + " << img << "i" << endl;
+    }
 };
-int main(){
-    Complex c1,c2,c3;
+
+int main() {
+    Complex c1, c2, c3;
+    int choice;
+
+    cout << "Complex Number Operations\n";
+    cout << "1. Addition\n";
+    cout << "2. Multiplication\n";
+    cout << "Enter your choice (1/2): ";
+    cin >> choice;
+
     c1.accept();
     c2.accept();
-    c3=c1+c2;
+
+    switch (choice) {
+        case 1:
+            c3 = c1 + c2;
+            cout << "Result of Addition: ";
+            break;
+        case 2:
+            c3 = c1 * c2;
+            cout << "Result of Multiplication: ";
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+            return 1;
+    }
 
     c3.Display();
     return 0;
